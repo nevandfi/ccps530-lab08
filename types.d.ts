054470e087d6eb1f96bbe0e3765b1e8b93711a49
nevandfi <47934419+nevandfi@users.noreply.github.com>
2025-03-12 15:52:05 -0400

    Initial project setup based on lab 7

---
+++ types.d.ts
@@ -0,0 +1,12 @@
+interface IBook {
+    title: string;
+    author: string;
+    publisher: string;
+    date: string;
+    website: string;
+}
+
+interface IPageOptions {
+    title: string;
+    lang: string;
+}
